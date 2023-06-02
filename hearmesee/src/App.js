f9b91933818585f9c7c32979b3bd8cf7d9fa9f52
jing-xiang <chewjingxiangjumps@gmail.com>
2023-06-02 23:01:36 +0800

    add coco

--- hearmesee/src/App.js
+++ hearmesee/src/App.js
@@ -1,47 +1,92 @@
-import React, { useRef, useEffect } from 'react';
-import Webcam from 'react-webcam';
+// Import dependencies
+import React, { useRef, useState, useEffect } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as cocossd from "@tensorflow-models/coco-ssd";
+import Webcam from "react-webcam";
+import "./App.css";
+import { drawRect } from "./utilities";
 
 function App() {
-  // Reference to webcam component 
   const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
 
-  // const capture = () => {
-  //   const imageSrc = webcamRef.current.getScreenshot();
-  //   console.log(imageSrc);
-  // };
-  // Function to capture an image detected from webcam 
-  // const capture = () => {
-  //   const imageSrc = webcamRef.current.getScreenshot();
-  //   console.log(imageSrc);
-  // };
+  // Main function
+  const runCoco = async () => {
+    const net = await cocossd.load();
+    console.log("Handpose model loaded.");
+    //  Loop and detect hands
+    setInterval(() => {
+      detect(net);
+    }, 10);
+  };
 
-  // Setting up of webcam access when component mounts
-  useEffect(() => {
-    navigator.mediaDevices.getUserMedia({ video: true })
-      .then((stream) => {
-        // Setting stream as source for webcam component
-        webcamRef.current.srcObject = stream;
-      })
-      .catch((error) => {
-        console.log('Error accessing webcam:', error);
-      });
-  }, []);
+  const detect = async (net) => {
+    // Check data is available
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
 
-  // Render app component 
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas height and width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // Make Detections
+      const obj = await net.detect(video);
+
+      // Draw mesh
+      const ctx = canvasRef.current.getContext("2d");
+      drawRect(obj, ctx); 
+    }
+  };
+
+  useEffect(()=>{runCoco()},[]);
+
   return (
     <div className="App">
-      <div className="video-container">
+      <header className="App-header">
         <Webcam
-          audio={false}
           ref={webcamRef}
-          screenshotFormat="image/jpeg"
-          className="webcam-video"
+          muted={true} 
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
         />
-      </div>
-      {/* <button onClick={capture}>Capture</button> */}
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 8,
+            width: 640,
+            height: 480,
+          }}
+        />
+      </header>
     </div>
   );
-  
 }
 
 export default App;
