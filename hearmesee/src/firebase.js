6164421299f9944b1183df8fa1ad7941e2fca477
avrilgk <avrilguok@gmail.com>
2023-07-19 01:13:53 +0800

    env

--- hearmesee/src/firebase.js
+++ hearmesee/src/firebase.js
@@ -4,19 +4,18 @@
   import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
 
-  // Your web app's Firebase configuration
+   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-    apiKey: "AIzaSyAeAl3FUH0PGDWToUzSqhKfj4af-RR-8ok",
-    authDomain: "hear-me-see.firebaseapp.com",
-    databaseURL: "https://hear-me-see-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "hear-me-see",
-    storageBucket: "hear-me-see.appspot.com",
-    messagingSenderId: "28925318865",
-    appId: "1:28925318865:web:77ec8aa82bf8c877a1d7f0",
-    measurementId: "G-WN6MKN9KBQ"
+    apiKey: process.env.REACT_APP_FIREBASE_API,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
   };
-
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
