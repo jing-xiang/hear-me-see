e2e23ac917f88e8aab687c54d368fe7f4a6e0369
avrilgk <avrilguok@gmail.com>
2023-07-20 23:26:00 +0800

    process env variables

--- hearmesee/src/firebase.js
+++ hearmesee/src/firebase.js
@@ -7,15 +7,16 @@
    // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-    apiKey: '${process.env.REACT_APP_FIREBASE_API}',
-    authDomain: '${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}',
-    databaseURL: '${process.env.REACT_APP_FIREBASE_DATABASE_URL}',
-    projectId: '${process.env.REACT_APP_FIREBASE_PROJECT_ID}',
-    storageBucket: '${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}',
-    messagingSenderId: '${process.env.REACT_APP_SENDER_ID}',
-    appId: '${process.env.REACT_APP_FIREBASE_ID}',
-    measurementId: '${process.env.REACT_APP_MEASUREMENT_ID}'
+    apiKey: `${process.env.REACT_APP_FIREBASE_API}`,
+    authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+    databaseURL: `${process.env.REACT_APP_FIREBASE_DATABASE_URL}`,
+    projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+    storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
+    messagingSenderId: `${process.env.REACT_APP_SENDER_ID}`,
+    appId: `${process.env.REACT_APP_FIREBASE_ID}`,
+    measurementId: `${process.env.REACT_APP_MEASUREMENT_ID}`
   };
+
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
@@ -74,8 +75,7 @@
         const errorMessage = error.message;
         console.error(errorCode, errorMessage);
         window.alert("Error occurred! " + errorMessage); 
-        // ..
-        window.alert("Error occurred! Try again.");
+   
       });
     }
   });
