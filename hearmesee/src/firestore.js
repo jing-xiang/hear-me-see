06dfedbab6f2dfd2795daa1c6314969ceb14b1be
avrilgk <avrilguok@gmail.com>
2023-07-20 23:43:45 +0800

    typo

--- hearmesee/src/firestore.js
+++ hearmesee/src/firestore.js
@@ -4,15 +4,16 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://support.google.com/firebase/answer/7015592
 const firebaseConfig = {
-    apiKey: '${process.env.REACT_APP_FIREBASE_API}',
-    authDomain: '${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}',
-    databaseURL: '${process.env.REACT_APP_FIREBASE_DATABASE_URL}',
-    projectId: '${process.env.REACT_APP_FIREBASE_PROJECT_ID}',
-    storageBucket: '${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}',
-    messagingSenderId: '${process.env.REACT_APP_SENDER_ID}',
-    appId: '${process.env.REACT_APP_FIREBASE_ID}',
-    measurementId: '${process.env.REACT_APP_MEASUREMENT_ID}'
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
